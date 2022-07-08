490dd30497a1c8abd4421db3f2b5ce0889f6d194
kaustavghosh <kaustavghosh2009@gmail.com>
2022-07-08 14:45:50 +0530

    Broadcast receiver

---
+++ app/src/main/java/com/kg/broadcastreceiverdemo/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.kg.broadcastreceiverdemo
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.kg.broadcastreceiverdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    // register the receiver in the main activity in order
+    // to receive updates of broadcasts events if they occur
+    lateinit var receiver: AirplaneModeChangeReceiver
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        receiver = AirplaneModeChangeReceiver()
+
+        // Intent Filter is useful to determine which apps wants to receive
+        // which intents,since here we want to respond to change of
+        // airplane mode
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // registering the receiver
+            // it parameter which is passed in  registerReceiver() function
+            // is the intent filter that we have just created
+            registerReceiver(receiver, it)
+        }
+    }
+
+    // since AirplaneModeChangeReceiver class holds a instance of Context
+    // and that context is actually the activity context in which
+    // the receiver has been created
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/kg/broadcastreceiverdemo/WifiStateReceiver.kt
@@ -0,0 +1,29 @@
+package com.kg.broadcastreceiverdemo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class WifiStateReceiver : BroadcastReceiver() {
+
+    // this function will be executed when the user changes his
+    // airplane mode
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        // intent contains the information about the broadcast
+        // in our case broadcast is change of airplane mode
+
+        // if getBooleanExtra contains null value,it will directly return back
+        val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
+
+        // checking whether airplane mode is enabled or not
+        if (isAirplaneModeEnabled) {
+            // showing the toast message if airplane mode is enabled
+            Toast.makeText(context, "Airplane Mode Enabled Kaustav", Toast.LENGTH_LONG).show()
+        } else {
+            // showing the toast message if airplane mode is disabled
+            Toast.makeText(context, "Airplane Mode Disabled Kaustav", Toast.LENGTH_LONG).show()
+        }
+    }
+}
